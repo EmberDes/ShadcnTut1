90aad8d49b190c1e1c704c1631ca7e85a5c5b99f
EmberDes <shashwatsinghdit@gmail.com>
2025-01-22 07:29:05 +0000

    DAY-3 Done Chai and shadcn

--- chai-aur-shadcn/src/app/chai/page.tsx
+++ chai-aur-shadcn/src/app/chai/page.tsx
@@ -1,11 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { DESTRUCTION } from 'dns'
 import React from 'react'
 
 function ChaiPage(){
     return (
         <main className='h-full flex justify-center items-center'>
             <div>ChaiPage</div>
+        
+        <Button variant={'destructive'}> shadcn Button</Button>
         </main>
-
     )
 }
 
